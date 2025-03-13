b4077bca3a39c3d02249a38aea47dbd59d35d912
PerpetualTechnologies <access@perpetualtech.ai>
2025-03-13 17:41:17 -0300

    adjusting yml

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,11 +1,11 @@
-variable "project_id" {
-  type        = string
-  description = "Google Cloud Project ID."
-}
-
 variable "service_account_email" {
   type        = string
   description = "Email service account."
+}
+
+variable "project_id" {
+  type        = string
+  description = "Google Cloud Project ID."
 }
 
 variable "bucket_name" {
--- terraform/variables.tfvars
+++ terraform/variables.tfvars
@@ -1,4 +1,4 @@
-#project_id                  = "your_default_project_id"
 #service_account_email       = "your_email_service_account"
+#project_id                  = "your_default_project_id"
 #bucket_name                 = "bucket_name"
 region_name = "us-west1"
